dcc5c7ed7126962e2842da05aff6273ce90e0f4c
Purnima <sharma0purnima@gmail.com>
2024-10-30 17:57:57 +0530

    added support for mongo example

--- src/main/java/com/opensourceFramework/utils/mongoDb/User.java
+++ src/main/java/com/opensourceFramework/utils/mongoDb/User.java
@@ -1,9 +1,42 @@
 package com.opensourceFramework.utils.mongoDb;
 
-import lombok.Data;
+
 import org.bson.types.ObjectId;
-@Data
+
 public class User {
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     private ObjectId id;
     private String name;
     private int age;
